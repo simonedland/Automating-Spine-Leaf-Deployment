d654a344eb67ead391662a09153a6301e12f0cc0
Simon Edland <Simon.vestvik2000@gmail.com>
2022-05-03 12:08:24 +0200

    cleaning up for the start

--- test.py
+++ test.py
@@ -1,21 +1,12 @@
 from nornir.core.task import Task, Result
 from nornir import InitNornir
-import portSecurer
 from nornir_utils.plugins.functions import print_result
 from nornir_netmiko.tasks import netmiko_send_command, netmiko_send_config
 import time
 
-
 #todo:
-#auto apply to routers
-#auto config ranges to dhcp server
-#adding the ability to spesify what leaf gets what range
-
-#future ideas for projekts
-#python bor
-#CDP based LAG constructor
-#auto secuing based on standards (dhcp snooping, dynamic arp inspection, port security, disabeling ports that is not needed)
-
+#1. basic connectivity test
+#2. make a enviroment reset function
 
 startTime=time.time() #this is the start time of the program
 
@@ -24,11 +15,10 @@ nr = InitNornir(config_file="config.yaml") #this is the nornir object
 singleHost = nr.filter(name="host1.cmh") #this is the nornir object with only one host
 
 def main():
-
-    #portSecurer.ARPMarker()
-    #reeee=nr.run(task=portSecurer.PortMarker, numberOfLayer2Ports=2)
-    reeee = singleHost.run(task=portSecurer.PortMarker, numberOfLayer2Ports=2)
-    #print_result(reeee)
+    print("test")
+    
 
 main() #run the main function
+
+
 print(f"\n\n\n\n\nthe script took {time.time()-startTime} seconds") #prints how long the script took to run
