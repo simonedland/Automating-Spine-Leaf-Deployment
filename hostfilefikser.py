f63dea6da313b33a0d80478d8785582f1956bf98
Simon Vestvik Edland <simon.vestvik2000@gmail.com>
2022-06-24 19:52:44 +0200

    think i got it

--- hostfilefikser.py
+++ hostfilefikser.py
@@ -1,15 +1,39 @@
 import yaml
 
+
+counter=1
+nr_of_spines=10
+nr_of_leafs=10
+
+
 mydict={}
-for x in range(1,10):
+for x in range(nr_of_spines):
     mydict.update({
-        f"spine{x}.cmh"
-        :{"hostname":x, "groups":[f"spine"]}
-
+        f"spine{counter}.cmh"
+        :{"hostname":f"10.100.0.{counter}", 
+        "groups":[f"spine"]}
         })
+    counter+=1
 
-d = {'A':'a', 'B':{'C':'c', 'D':'d', 'E':'e'}}
-print(d)
+
+y=0
+z=1
+for x in range(nr_of_leafs):
+    if y == 2:
+        z+=1
+        y=0
+
+    mydict.update({
+        f"leaf{x+1}.cmh"
+        :{"hostname":f"10.100.0.{counter}", 
+        "groups":[f"leaf"], 
+        "data":{"switchpair":z}}
+        })
+    counter+=1
+
+    y+=1
+
+
 print(mydict)
 with open('result.yml', 'w') as yaml_file:
     yaml_file.write("---\n")
