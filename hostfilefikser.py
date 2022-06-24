7b29bbea7d274ff7a7464ca6fc7871013c77277d
Simon Vestvik Edland <simon.vestvik2000@gmail.com>
2022-06-24 20:09:04 +0200

    implemented edge

--- hostfilefikser.py
+++ hostfilefikser.py
@@ -1,20 +1,23 @@
 import yaml
 
+#this is a hostfile generator
+#it works uto 254 hosts in total
+#edge nodes are placed in the last switchpair
 
 counter=1
-nr_of_spines=10
+nr_of_spines=3
 nr_of_leafs=10
 
+hostname3octets="10.100.0."
 
 mydict={}
 for x in range(nr_of_spines):
     mydict.update({
         f"spine{counter}.cmh"
-        :{"hostname":f"10.100.0.{counter}", 
+        :{"hostname":f"{hostname3octets}{counter}", 
         "groups":[f"spine"]}
         })
     counter+=1
-
 
 y=0
 z=1
@@ -23,16 +26,23 @@ for x in range(nr_of_leafs):
         z+=1
         y=0
 
-    mydict.update({
+    if x >= nr_of_leafs-2:
+        mydict.update({
         f"leaf{x+1}.cmh"
-        :{"hostname":f"10.100.0.{counter}", 
+        :{"hostname":f"{hostname3octets}{counter}", 
+        "groups":[f"leaf", "edge"], 
+        "data":{"switchpair":z}}
+        })
+    else:
+        mydict.update({
+        f"leaf{x+1}.cmh"
+        :{"hostname":f"{hostname3octets}{counter}", 
         "groups":[f"leaf"], 
         "data":{"switchpair":z}}
         })
+
     counter+=1
-
     y+=1
-
 
 print(mydict)
 with open('result.yml', 'w') as yaml_file:
