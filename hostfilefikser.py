76281199885d18726ec218eab345fbaea0b0ee30
Simon Vestvik Edland <simon.vestvik2000@gmail.com>
2022-06-24 19:22:46 +0200

    spine format done

--- hostfilefikser.py
+++ hostfilefikser.py
@@ -1,16 +1,16 @@
 import yaml
 
 mydict={}
-for x in range(100):
-    print (x)
+for x in range(1,10):
     mydict.update({
         f"spine{x}.cmh"
-        :{x:x}
+        :{"hostname":x, "groups":[f"spine"]}
+
         })
 
 d = {'A':'a', 'B':{'C':'c', 'D':'d', 'E':'e'}}
 print(d)
-
+print(mydict)
 with open('result.yml', 'w') as yaml_file:
     yaml_file.write("---\n")
     yaml.dump(mydict, yaml_file)
