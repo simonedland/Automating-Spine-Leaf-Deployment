66bdf13a74e1084add0b4309d5a94f087f3a7fcd
Simon Vestvik Edland <simon.vestvik2000@gmail.com>
2022-06-25 01:06:26 +0200

    scaled up to be able to use 10 x 55

--- meshlistexample.py
+++ meshlistexample.py
@@ -1,10 +1,13 @@
 #tunnelLan=[]
-NrOfSpines=3
-NrOfLeafs=20
-scale=4
+NrOfSpines=10
+NrOfLeafs=55
+scale=25
 Oct3Offset=0
 
 from Subbnetter import subbnetter
+from tqdm import tqdm
+
+PB=tqdm(total=NrOfLeafs*NrOfSpines*NrOfLeafs)
 
 #for x in range(0,NrOfSpines): #not very modular and not very efficient in adition not very elegant either
 #    tunnelLan.append(subbnetter(nettwork=f"10.3.{x*scale}.0",nettworkReq=[{"numberOfSubbnets":NrOfLeafs, "requiredHosts":2},]))
@@ -27,14 +30,16 @@ for Dim1 in range(0,NrOfLeafs): #this makes the ip list for the leafs
         NewIpList[Dim1].append([]) #append a new list to the list
         Horisontal=False #this is the horizontal flag that changes the ip grid to use new ips when passing trough the tunnel that should have pointed to ti self
         for Dim3 in range(0,NrOfLeafs): #adds the last dimension to the ip list that is asosiated with the number of leafs
+            PB.update()
+
             if Dim3 == Dim1: #if the ip is on the same row as the leaf meaning it is on the same tunnel (pointing to self)
                 NewIpList[Dim1][Dim2].append("nan") #adds a nan to the ip list
                 
-                iteration+=1
-                print(iteration)
-                for x in NewIpList:
-                    print(x)
-                print("\n")
+                #iteration+=1
+                #print(iteration)
+                #for x in NewIpList:
+                #    print(x)
+                #print("\n")
 
                 Horisontal=True #sets the horizontal flag to true
             else: #if the ip is not on the same row as the leaf meaning it is on a different tunnel (pointing to another leaf)
@@ -42,11 +47,11 @@ for Dim1 in range(0,NrOfLeafs): #this makes the ip list for the leafs
                     if NewIpList[Dim1][Dim2][-1]=="nan": #if the last ip is a nan meaning that it is the beginning of the list
                         NewIpList[Dim1][Dim2].append(f"{Dim2*scale+Oct3Offset}.1") #adds the ip to the list
 
-                        iteration+=1
-                        print(iteration)
-                        for x in NewIpList:
-                            print(x)
-                        print("\n")
+                        #iteration+=1
+                        #print(iteration)
+                        #for x in NewIpList:
+                        #    print(x)
+                        #print("\n")
 
                     else: #if the last ip is not a nan meaning that it is not the beginning of the list
                         prev = NewIpList[Dim1][Dim2][-1] #sets the prev variable to the last ip in the list
@@ -58,12 +63,12 @@ for Dim1 in range(0,NrOfLeafs): #this makes the ip list for the leafs
                         else:
                             NewIpList[Dim1][Dim2].append(f"{prev.split('.')[0]}.{int(prev.split('.')[1])+4}") #adds the ip to the list and adds 4 to the ip
 
-                        iteration+=1
-                        print(iteration)
-                        for x in NewIpList:
-                            print(x)
-                        print("\n")
-
+                        #iteration+=1
+                        #print(iteration)
+                        #for x in NewIpList:
+                        #    print(x)
+                        #print("\n")
+#
                 else: #if the ip is not on the first row meaning it is on a row that is not the first leaf
                     if Horisontal: #if it needs to create new ips bechause it passed itself
                         if NewIpList[Dim1][Dim2][-1]=="nan": #it it just passed
@@ -76,15 +81,13 @@ for Dim1 in range(0,NrOfLeafs): #this makes the ip list for the leafs
                             else:
                                 NewIpList[Dim1][Dim2].append(f"{NewIpList[Dim1-1][Dim2][-1].split('.')[0]}.{int(NewIpList[Dim1-1][Dim2][-1].split('.')[1])+4}") #takes the last ip of the previous leaf and adds 4 to it
                             
-                            iteration+=1
-                            print(iteration)
-                            for x in NewIpList:
-                                print(x)
-                            print("\n")
+                            #iteration+=1
+                            #print(iteration)
+                            #for x in NewIpList:
+                            #    print(x)
+                            #print("\n")
 
                         else: #if it is not just passed
-                            print(int(NewIpList[Dim1][Dim2][-1].split('.')[1])+4)
-
                             if int(NewIpList[Dim1][Dim2][-1].split('.')[1]) == 253:
                                 if offsetTimeOute==False:
                                     Oct3Offset+=1
@@ -93,20 +96,20 @@ for Dim1 in range(0,NrOfLeafs): #this makes the ip list for the leafs
                             else:
                                 NewIpList[Dim1][Dim2].append(str(int(NewIpList[Dim1][Dim2][-1].split('.')[0]))+"."+str(int(NewIpList[Dim1][Dim2][-1].split('.')[1])+4)) #adds 4 to the last ip of the list
                             
-                            iteration+=1
-                            print(iteration)
-                            for x in NewIpList:
-                                print(x)
-                            print("\n")
+                            #iteration+=1
+                            #print(iteration)
+                            #for x in NewIpList:
+                            #    print(x)
+                            #print("\n")
                             
                     else: #if it has another leaf to connect to
                         NewIpList[Dim1][Dim2].append(NewIpList[Dim3][Dim2][Dim1].split('.')[0]+"."+str(int(NewIpList[Dim3][Dim2][Dim1].split('.')[1])+1)) #adds 1 to the last ip of the list choosinga higher ip in the microsegment
                         
-                        iteration+=1
-                        print(iteration)
-                        for x in NewIpList:
-                            print(x)
-                        print("\n")
+                        #iteration+=1
+                        #print(iteration)
+                        #for x in NewIpList:
+                        #    print(x)
+                        #print("\n")
                         
 #for x in tunnelLan:
 #    for y in x:
