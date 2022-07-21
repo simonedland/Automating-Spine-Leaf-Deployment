80eb7d00ed19a5d6c48d4141c8bf53b4d3b88c89
Sion VEstvik Edland <Simon.Vestvik2000@gmail.com>
2022-07-21 22:14:11 +0200

    just testing the parameters to see if ok

--- subbnettester.py
+++ subbnettester.py
@@ -11,7 +11,7 @@ from NEW_subbnetter import subbnetter
 test = subbnetter(nettwork=f"192.168.1.123",nettworkReq=[
             #{"numberOfSubbnets":1, "requiredHosts":254*254},
             #{"numberOfSubbnets":1, "requiredHosts":254},
-            {"numberOfSubbnets":10, "requiredHosts":700}
+            {"numberOfSubbnets":10, "requiredHosts":1100}
             #{"numberOfSubbnets":20, "requiredHosts":30}
             ])
 print(test)
