73cfc8421eb82cd134e530fc5026349cc4a25f05
Sion VEstvik Edland <Simon.Vestvik2000@gmail.com>
2022-07-30 19:10:41 +0200

    rewind again and try again

--- subbnettester.py
+++ subbnettester.py
@@ -1,17 +1,17 @@
 from Subbnetter import subbnetter
 
-test = subbnetter(nettwork=f"192.168.1.0",nettworkReq=[
-            {"numberOfSubbnets":2, "requiredHosts":2},
-            {"numberOfSubbnets":2, "requiredHosts":5}
+test = subbnetter(nettwork=f"192.255.255.0",nettworkReq=[
+            {"numberOfSubbnets":20, "requiredHosts":122},
+            #{"numberOfSubbnets":2, "requiredHosts":5}
             ])
 #print(test)
 
 from NEW_subbnetter import subbnetter
 
-test = subbnetter(nettwork=f"192.168.1.123",nettworkReq=[
-            #{"numberOfSubbnets":1, "requiredHosts":254*254},
-            #{"numberOfSubbnets":1, "requiredHosts":254},
-            {"numberOfSubbnets":10, "requiredHosts":1100}
-            #{"numberOfSubbnets":20, "requiredHosts":30}
+test = subbnetter(nettwork=f"192.168.10.141",nettworkReq=[
+            {"numberOfSubbnets":2, "requiredHosts":2},
+            {"numberOfSubbnets":4, "requiredHosts":254},
+            {"numberOfSubbnets":10, "requiredHosts":254*254*4},
+            #{"numberOfSubbnets":3, "requiredHosts":254*254*254}
             ])
 print(test)
