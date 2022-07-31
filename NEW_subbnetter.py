e1075fbdd36bb9885ba18d0b3a697980f6515f04
Sion VEstvik Edland <Simon.Vestvik2000@gmail.com>
2022-07-31 22:50:59 +0200

    finishing up

--- NEW_subbnetter.py
+++ NEW_subbnetter.py
@@ -25,7 +25,6 @@ def subbnetter(nettwork, nettworkReq):
                 host_bit_value=octetValueList[i]
                 i+=1
             
-
             #find the sbnetID
             host_bit_length=len(bin(host_bit_value)[2:])
 
@@ -36,14 +35,14 @@ def subbnetter(nettwork, nettworkReq):
                 if new_octet_int+host_bit_value>=256: #if octet four overflows
                     if int(nettwork.split(".")[2])+1>=256:
                         if int(nettwork.split(".")[1])+1>=256:
-                            new_nettwork=f"{int(nettwork.split('.')[0])+1}.0.0.0"
+                            new_nettwork_ID=f"{int(nettwork.split('.')[0])+1}.0.0.0"
                         else:
-                            new_nettwork=f"{nettwork.split('.')[0]}.{int(nettwork.split('.')[1])+1}.0.0"
+                            new_nettwork_ID=f"{nettwork.split('.')[0]}.{int(nettwork.split('.')[1])+1}.0.0"
                     else:
-                        new_nettwork=f"{nettwork.split('.')[0]}.{nettwork.split('.')[1]}.{int(nettwork.split('.')[2])+1}.0"
+                        new_nettwork_ID=f"{nettwork.split('.')[0]}.{nettwork.split('.')[1]}.{int(nettwork.split('.')[2])+1}.0"
                 else:
-                    new_nettwork=f"{nettwork.split('.')[0]}.{nettwork.split('.')[1]}.{nettwork.split('.')[2]}.{new_octet_int+host_bit_value}"
-
+                    new_nettwork_ID=f"{nettwork.split('.')[0]}.{nettwork.split('.')[1]}.{nettwork.split('.')[2]}.{new_octet_int+host_bit_value}"
+                nettwork=f"{nettwork.split('.')[0]}.{nettwork.split('.')[1]}.{nettwork.split('.')[2]}.{new_octet_int}"
 
             elif host_bit_length>8 and host_bit_length<=16:
                 octet_bits=bin(int(nettwork.split(".")[2]))[2:]
@@ -54,12 +53,12 @@ def subbnetter(nettwork, nettworkReq):
                     new_octet_int=int(f"{octet_bits}",2)
                 if (host_bit_value/256)+new_octet_int==256: #if octet three overflows
                     if int(nettwork.split('.')[1])+1==256:
-                        new_nettwork=f"{int(nettwork.split('.')[0])+1}.0.0.0"
+                        new_nettwork_ID=f"{int(nettwork.split('.')[0])+1}.0.0.0"
                     else:
-                        new_nettwork=f"{nettwork.split('.')[0]}.{int(nettwork.split('.')[1])+1}.0.0"
+                        new_nettwork_ID=f"{nettwork.split('.')[0]}.{int(nettwork.split('.')[1])+1}.0.0"
                 else:
-                    new_nettwork=f"{nettwork.split('.')[0]}.{nettwork.split('.')[1]}.{new_octet_int+(int(host_bit_value/256))}.0"
-
+                    new_nettwork_ID=f"{nettwork.split('.')[0]}.{nettwork.split('.')[1]}.{new_octet_int+(int(host_bit_value/256))}.0"
+                nettwork=f"{nettwork.split('.')[0]}.{nettwork.split('.')[1]}.{new_octet_int}.0"
 
             elif host_bit_length>16 and host_bit_length<=24:
                 octet_bits=bin(int(nettwork.split(".")[1]))[2:]
@@ -69,10 +68,11 @@ def subbnetter(nettwork, nettworkReq):
                 else:
                     new_octet_int=int(f"{octet_bits}",2)
                 if (host_bit_value/256/256)+new_octet_int==256: #if octet two overflows
-                    new_nettwork=f"{int(nettwork.split('.')[0])+1}.0.0.0"
+                    nettwork=f"{int(nettwork.split('.')[0])}.{new_octet_int}.0.0"
+                    new_nettwork_ID=f"{int(nettwork.split('.')[0])+1}.0.0.0"
                 else:
-                    new_nettwork=f"{nettwork.split('.')[0]}.{new_octet_int+(int(host_bit_value/(256*256)))}.0.0"
-
+                    new_nettwork_ID=f"{nettwork.split('.')[0]}.{new_octet_int+(int(host_bit_value/(256*256)))}.0.0"
+                nettwork=f"{nettwork.split('.')[0]}.{new_octet_int}.0.0"
 
             elif host_bit_length>24 and host_bit_length<=32:
                 octet_bits=bin(int(nettwork.split(".")[0]))[2:]
@@ -81,21 +81,41 @@ def subbnetter(nettwork, nettworkReq):
                     new_octet_int=int(f"{new_octet_bits}",2)
                 else:
                     new_octet_int=int(f"{octet_bits}",2)
-                new_nettwork=f"{new_octet_int+(int(host_bit_value/(256*256*256)))}.0.0.0"
+                nettwork=f"{new_octet_int}.0.0.0"
+                new_nettwork_ID=f"{new_octet_int+(int(host_bit_value/(256*256*256)))}.0.0.0"
 
 
 
+            subbnetBittList=[0,128,192,224,240,248,252,254,255] #list of subbnet bits
+            octet1,octet2,octet3,octet4=0,0,0,0 #octet values
+            maskList=[0,0,0,0] #list of mask values
+            for i in range(33-host_bit_length): #for each bit
+                if maskList[0]==255: #if the mask is 255
+                    if maskList[1]==255: #if the mask is 255
+                        if maskList[2]==255: #if the mask is 255
+                            if maskList[3]==255: #if the mask is 255
+                                pass
+                            else: #if the mask is not 255
+                                octet4+=1 #adds 1 to the mask
+                                maskList[3]=subbnetBittList[octet4] #sets the mask to the subbnet bit
+                        else:
+                            octet3+=1
+                            maskList[2]=subbnetBittList[octet3]
+                    else:
+                        octet2+=1
+                        maskList[1]=subbnetBittList[octet2]
+                else:
+                    octet1+=1
+                    maskList[0]=subbnetBittList[octet1]
+            mask=f"{maskList[0]}.{maskList[1]}.{maskList[2]}.{maskList[3]}" #sets the mask
 
-
-
-            nettwork=new_nettwork
-
-            print(new_nettwork, host_bit_value)
+            networkList.append({"subbnetID":subbnetID, "broadcast":broadcast, "mask":mask, "nrOfHostsAvaidable":f"{octetvale}"})
+            print(nettwork)
+            nettwork=new_nettwork_ID
+            
+            print(new_nettwork_ID, mask)
             print("\n")
-            #do the subbnetting
-
-            #make that the new subbnet id
-
+            
 
     print(bin(25)[2:])
     print(nettworkReq)
--- subbnettester.py
+++ subbnettester.py
@@ -9,9 +9,9 @@ test = subbnetter(nettwork=f"192.255.255.0",nettworkReq=[
 from NEW_subbnetter import subbnetter
 
 test = subbnetter(nettwork=f"192.169.10.141",nettworkReq=[
-            {"numberOfSubbnets":4, "requiredHosts":126},
-            {"numberOfSubbnets":30, "requiredHosts":254*128},
-            {"numberOfSubbnets":60, "requiredHosts":254*254*4},
-            {"numberOfSubbnets":3, "requiredHosts":254*254*254}
+            {"numberOfSubbnets":4, "requiredHosts":2},
+            {"numberOfSubbnets":3, "requiredHosts":254},
+            {"numberOfSubbnets":1, "requiredHosts":254*254*4},
+            {"numberOfSubbnets":1, "requiredHosts":254*254*254*5}
             ])
 print(test)
